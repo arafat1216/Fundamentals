a6d48dbf25dd2ce4a70a2ccdf89e1f23333e24f3
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-08 22:25:55 +0600

    Perform arithmetic and bitwise opetaions

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,7 +11,25 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
-           
+            int number1 = 10, number2 = 3;
+
+            int sum = number1 + number2;
+            int sub = number1 - number2;
+            int product = number1 * number2;
+            double division = (double)number1/number2;
+            int remainder = number1 % number2;
+            var bitWiseOR = number1 | number2;
+            var bitWiseAND = number1 % number2;
+            var bitWiseXOR = number1 ^ number2;
+
+            Console.WriteLine($"{number1} + {number2} = {sum}");
+            Console.WriteLine($"{number1} - {number2} = {sub}");
+            Console.WriteLine($"{number1} * {number2} = {product}");
+            Console.WriteLine($"{number1} / {number2} = {division:N2}");
+            Console.WriteLine($"{number1} % {number2} = {remainder}");
+            Console.WriteLine($"{number1} | {number2} = {bitWiseOR}");
+            Console.WriteLine($"{number1} & {number2} = {bitWiseAND}");
+            Console.WriteLine($"{number1} ^ {number2} = {bitWiseXOR}");
         }
     }
 }
