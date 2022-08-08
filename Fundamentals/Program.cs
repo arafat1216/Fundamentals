4254d7417545bd60e41edcd93a82426c1bb2e1dd
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-08 21:03:35 +0600

    Add primitive types

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,7 +11,21 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
-           
+            int daysSinceLastLoggedIn = 12;
+            double interestRate = 1.5;
+            float yearsOfExperience = 2.5f;
+            long productiveTimeInSeconds = 79349367648374;
+            bool isLoggedIn = false;
+            char symbol = 'y';
+            string name = "Mohammad Arafat Hossain Anik";
+
+            Console.WriteLine($"Type: Int, Value: {daysSinceLastLoggedIn}");
+            Console.WriteLine($"Type: Double, Value: {interestRate}");
+            Console.WriteLine($"Type: Float, Value: {yearsOfExperience}");
+            Console.WriteLine($"Type: Long, Value: {productiveTimeInSeconds}");
+          Console.WriteLine($"Type: Boolean, Value: {isLoggedIn}");
+            Console.WriteLine($"Type: Char, Value: {symbol}");
+            Console.WriteLine($"Type: String, Value: {name}");
         }
     }
 }
