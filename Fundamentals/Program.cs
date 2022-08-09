30005a2ca7078655228612e2c7ef4f38b6476397
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-10 00:23:56 +0600

    Calculate max using ternary operator

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,6 +11,16 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter 1st Number");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd Number");
+            int number2 = int.Parse(Console.ReadLine());
+
+            int max = number1>number2 ? number1 : number2;
+
+            Console.WriteLine($"Max: {max}");
+          
            
         }
     }
