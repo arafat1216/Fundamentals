e2b21daff3857c00c1b781f209cb2630be0ccbb2
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-09 23:55:05 +0600

    Implement a program that detects leap year

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,7 +11,25 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Enter A Year");
+            int year = int.Parse(Console.ReadLine());
+
+            if(year%400==0)
+            {
+                Console.WriteLine($"{year} Is A Leap Year");
+            }
+            else if(year%100==0)
+            {
+                Console.WriteLine($"{year} Is Not A Leap Year");
+            }
+            else if(year%4==0)
+            {
+                Console.WriteLine($"{year} Is A Leap Year");
+            }
+            else
+            {
+                Console.WriteLine($"{year} Is Not A Leap Year");
+            }
         }
     }
 }
