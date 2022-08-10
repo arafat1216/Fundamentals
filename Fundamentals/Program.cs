fbbe745c1e7c6cefd96e999ebdafe97d08c398a8
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-10 15:55:14 +0600

    Print 1 to 5 on console

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,7 +11,13 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
-           
+            int i = 1;
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= 5);
         }
     }
 }
