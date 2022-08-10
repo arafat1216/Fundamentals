88b83e50d6105c721be93cd3f803ccd932ebe18b
Arafat Hossain <arafathossain0895@gmail.com>
2022-08-10 15:45:36 +0600

    Print sum of digits of a given integer

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -11,7 +11,27 @@ namespace Fundamentals
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("Enter A Number");
+            int number = int.Parse(Console.ReadLine());
+
+            
+            SumofDigits(number);
+        }
+
+        /// <summary>
+        ///     Returns Sum of Digits of A Given Number.
+        /// </summary>
+        /// <param name="number"> Takes An Integer. </param>
+        static void SumofDigits(int number)
+        {
+            int sum = 0;
+            while (number > 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            Console.WriteLine($"SUM OF Digits: {sum}");
         }
     }
 }
